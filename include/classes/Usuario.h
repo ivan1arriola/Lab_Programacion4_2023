7456aa394e6260fd4feaa2d5e83874f08daf8a65
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-05-28 14:23:09 -0300

    falto virtual

--- include/classes/Usuario.h
+++ include/classes/Usuario.h
@@ -23,25 +23,25 @@ class Usuario {
     virtual Usuario() {};
 
     // Getters
-    string getNickname() = 0;
-    string getContrasenia() = 0;
-    string getNombre() = 0;
-    string getDescripcion() = 0;
-    vector<DTNotificacion> getNotificacionesRecibidas() = 0;
+    virtual string getNickname() = 0;
+    virtual string getContrasenia() = 0;
+    virtual string getNombre() = 0;
+    virtual string getDescripcion() = 0;
+    virtual vector<DTNotificacion> getNotificacionesRecibidas() = 0;
 
     // Setters
-    void setNickname(string nickname) = 0;
-    void setContrasenia(string contrasenia) = 0;
-    void setNombre(string nombre) = 0;
-    void setDescripcion(string descripcion) = 0;
+    virtual void setNickname(string nickname) = 0;
+    virtual void setContrasenia(string contrasenia) = 0;
+    virtual void setNombre(string nombre) = 0;
+    virtual void setDescripcion(string descripcion) = 0;
 
     // Operaciones
-    void enviarNotificacion(string nombreCurso, string nombreIdioma) = 0;
-    bool esProfesor() = 0;
-    set<string> obtenerIdiomas() = 0;
-    void eliminarNotificacionesRecibidas() = 0;
-    bool esEstudiante() = 0;
-    set<DataCursoProfesor> getCursosProfesor() = 0;
+    virtual void enviarNotificacion(string nombreCurso, string nombreIdioma) = 0;
+    virtual bool esProfesor() = 0;
+    virtual set<string> obtenerIdiomas() = 0;
+    virtual void eliminarNotificacionesRecibidas() = 0;
+    virtual bool esEstudiante() = 0;
+    virtual set<DataCursoProfesor> getCursosProfesor() = 0;
 
     virtual ~Usuario() {}
 
