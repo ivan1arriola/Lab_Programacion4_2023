694506056deb34e7c36eda13d17ce3824530b762
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-05-28 14:23:25 -0300

    Estudiante

---
+++ include/classes/Estudiante.h
@@ -0,0 +1,33 @@
+#ifndef ESTUDIANTE
+#define ESTUDIANTE
+
+#include <string>
+
+#include "Usuario.h"
+#include "../datatypes/DTDate.h"
+
+using namespace std;
+
+class Estudiante : public Usuario {
+    private:
+        string pais;
+        DTDate fechaNacimiento;
+    public:
+        Estudiante();
+        Estudiante(string nickname, string contrasenia, string nombre, string descripcion, string pais, DTDate fechaNacimiento);
+
+        // Getters
+        string getPais();
+        DTDate getFechaNacimiento();
+
+        // Setters
+        void setPais(string pais);
+        void setFechaNacimiento(DTDate fechaNacimiento);
+        void setFechaNacimiento(int dia, int mes, int anio);
+
+        // Operaciones
+        void enviarNotificacion(string nombreCurso, string nombreIdioma);
+
+        ~Estudiante();
+        friend ostream& operator<<(ostream& os, const Estudiante& estudiante);
+};
