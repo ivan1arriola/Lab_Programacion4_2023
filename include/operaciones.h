1533a34f69391e9c7b28c378025c2982a1bf0adf
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-05-31 15:46:16 -0300

    descripciones de las operaciones

--- include/operaciones.h
+++ include/operaciones.h
@@ -4,17 +4,28 @@
 #include <set>
 #include <string>
 
+using namespace std;
 
-void imprimirSet(const std::set<std::string>& conjunto);
+void imprimirSet(const set<string>& conjunto);
 
+/* Imprime una linea de guiones, un string, otra linea de guiones y un salto de
+ * linea */
+void imprimirMensaje(string mensaje);
+
+/* Imprime un mensaje de bienvenida */
 void mensajeDeBienvenida();
 
+/* Imprime una lista de todos los casos de uso disponibles */
 void menuDeOpciones();
 
-/** Lee un numero por consola. Si no es un numero, lo vuelve a pedir. */
-/** Siempre agrega la opcion 0*/
+/* Lee un numero por consola.Si es un numero, verifica que sea una opcion valida
+ * (entre 0 y cantOpciones). Si no es un numero, pide que se ingrese un numero.
+ * Si es un numero pero no es una opcion valida, pide que se ingrese una opcion
+ * valida. Devuelve la opcion elegida. */
 int ingresarOpcion(int cantOpciones);
 
+/* Pregunta si desea continuar. Devuelve 1 si desea continuar, 0 si no desea
+ * continuar */
 int deseaContinuar();
 
 #endif
