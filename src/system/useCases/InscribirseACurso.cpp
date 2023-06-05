b03ac3a995681579d26769cc30cd51c063191cfc
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-06-04 21:13:07 -0300

    borre include s no necesarios

--- src/system/useCases/InscribirseACurso.cpp
+++ src/system/useCases/InscribirseACurso.cpp
@@ -3,12 +3,7 @@
 
 #include "../../../include/system/Sistema.h"
 
-#include "../../../include/factory/fabrica.h"
 #include "../../../include/system/operaciones.h"
-
-#include "../../../include/interfaces/IControladorCurso.h"
-#include "../../../include/interfaces/IControladorEstadistica.h"
-#include "../../../include/interfaces/IControladorUsuario.h"
 
 using namespace std;
 
--- src/system/useCases/RealizarEjercicio.cpp
+++ src/system/useCases/RealizarEjercicio.cpp
@@ -3,7 +3,6 @@
 
 #include "../../../include/system/Sistema.h"
 
-#include "../../../include/factory/fabrica.h"
 #include "../../../include/system/operaciones.h"
 
 using namespace std;
